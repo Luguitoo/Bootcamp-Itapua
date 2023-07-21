0ac15ba07961f680f1f1b31d1c616dabb54ab7f4
Carlos Lugo <lugostein201@gmail.com>
2023-07-21 12:20:26 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Bootcamp-Itapua
+ Desarrollo Joven - Bootcamp Itapua
---
+++ dic_caffe/script.js
@@ -0,0 +1,27 @@
+alert("Hello, JavaScript");
+// comentario
+
+// adición
+console.log(5 + 3);
+
+// sustracción
+console.log(5 - 3);
+
+// multiplicación
+console.log(3 * 2);
+
+// división (arith)
+console.log(6 / 3);
+
+// Potencia al cuadrado (abajo es 5 al cuadrado)
+console.log(5 ** 2);
+
+//declarar variables
+let width = 100;
+let height = 50;
+let area = width * height;
+console.log(area);
+
+function clickAlert() {
+    alert("Pulsado.");
+  }
